18a9a32db2a27abe1e4f7d0b9feaf9e67d5c28e6
Thomas Hamilton <1581375@cegep-heritage.qc.ca>
2026-09-22 10:28:33 -0400

    Initial Creation of C# MVC environment

---
+++ TH_H60_A01/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using TH_H60_A01.Models;
+
+namespace TH_H60_A01.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
